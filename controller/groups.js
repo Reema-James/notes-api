784db33042904a72b3a6c2cc4d48ed86db48a816
Reema-James <97081781+Reema-James@users.noreply.github.com>
2022-06-17 11:28:55 +0530

    Today's update

--- controller/groups.js
+++ controller/groups.js
@@ -2,6 +2,7 @@ import {v4 as uuid} from "uuid";
 
 let groups = [];
 let notes =[];
+let favourites=[];
 
 
 export const getGroups = (req,res) =>{
@@ -21,6 +22,90 @@ export const getNotes = (req,res) => {
 export const createNote = (req,res) => {
     const note = req.body;
     notes.push({...note, id: uuid()});
-    res.send("Response added Sucessfully");
+    res.send("Response added Successfully");
 };
 
+export const getFavourites = (req,res) =>{
+    for(let i=0; i<notes.length; i++)
+    if (notes[i].favourites==1){
+        res.send(notes[i]);
+    }
+    
+};
+
+
+export const deleteGroup=(req,res)=>{
+    let idnew=req.params.id;
+    
+    for(let i=0;i<groups.length;i++)
+    {
+
+        
+       if(idnew==":"+groups[i].id)
+         
+             groups.splice(i,1);
+
+            }
+            return res.json({success:200, message:'The selected Group has been deleted'})
+            }
+        
+        
+            
+        
+        export const deleteNote=(req,res)=>{
+            let idnew=req.params.id;
+            
+            for(let i=0;i<notes.length;i++)
+            {
+                if(idnew==":"+notes[i].id)
+         
+                groups.splice(i,1);
+    
+           }
+       return res.json({success:200, message:'The selected Note has been deleted'})
+   }
+   
+   
+   export const updateGroup=(req,res)=>{
+       let idnew=req.params.id;
+       
+       for(let i=0;i<groups.length;i++)
+       {
+        if(idnew==":"+groups[i].id)
+         
+             groups.splice(i,1);
+ 
+        }
+    
+    
+   
+
+     let group=req.body;
+     groups.push({...group, id: idnew});
+
+
+    return res.json({success:200, message:'The selected Group has been Updated'})
+}
+
+export const updateNote=(req,res)=>{
+    let idnew=req.params.id;
+    for(let i=0;i<groups.length;i++)
+    {
+
+        
+       if(idnew==":"+groups[i].id)
+         
+             groups.splice(i,1);
+ 
+        }
+    
+    
+   
+
+     let group=req.body;
+     groups.push({...group, id: idnew});
+
+
+    return res.json({success:200, message:'The selected Note has been Updated'})
+}
+        
--- routes/groups.js
+++ routes/groups.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createGroup, createNote, getGroups, getNotes } from "../controller/groups.js"
+import { createGroup, createNote, getFavourites, getGroups, getNotes, deleteGroup, deleteNote, updateGroup, updateNote } from "../controller/groups.js"
 
 const router = express.Router();
 
@@ -7,5 +7,14 @@ router.get("/groups", getGroups);
 router.post("/group", createGroup);
 router.get("/notes", getNotes);
 router.post("/note",createNote);
+router.get("/favourites", getFavourites);
+
+
+router.delete( `/groups:id`, deleteGroup);
+router.delete( `/notes:id`, deleteNote);
+
+router.put( `/groups:id`, updateGroup);
+router.put( `/notes:id`, updateNote);
+
 
 export default router;
