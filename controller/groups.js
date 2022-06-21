a1cdad584e94d213fb9aa96805bfcb6bccf22852
Reema-James <97081781+Reema-James@users.noreply.github.com>
2022-06-21 16:53:45 +0530

    21-06-2022

--- controller/groups.js
+++ controller/groups.js
@@ -9,14 +9,18 @@ export const getGroups = (req,res) =>{
     res.send(groups);
 };
 
+
 export const createGroup = (req,res) => {
     const group = req.body;
-    groups.push({...group, id: uuid(), count: 0});
-    res.send("Response added Sucessfully");
+    const id= uuid();
+    groups.push({...group, id: id, count: 0});
+    map.set(id, []);
+    res.send(groups);
 };
 
+
 export const getNotes = (req,res) => {
-    let id=String(req.params.id).slice(1);
+    let id = String(req.params.id).slice(1);
     if(map.has(id)){
         res.send(map.get(id));
     }
@@ -24,116 +28,102 @@ export const getNotes = (req,res) => {
 };
 
 export const getNotesCount = (req,res) => {
-    let id=String(req.params.id).slice(1);
+    let id = String(req.params.id).slice(1);
     if(map.has(id)){
         res.send(map.get(id).length);
     }
-
     res.send(0);
 };
 
 export const createNote = (req,res) => {
-    let groupId=String(req.params.id).slice(1);
+    let groupId = String(req.params.id).slice(1);
     const note = req.body;
-    const id=uuid();
-    const newNote= {...note, id, groupId, favourite:false};
-    for(let i = 0; i < groups.length; i++) {
+    const id = uuid();
+    const newNote = {...note, id, groupId, favourite:false};
+    for (let i = 0; i < groups.length; i++) {
         let group = groups[i];
         if (group.id === groupId) {
            group.count = group.count + 1; 
         }
         groups[i] = group;
     }
-    if(map.has(groupId)){
+    if (map.has(groupId)) {
         map.get(groupId).push(newNote);
-    }
-
-    else{
-        const newNoteList=[newNote];
-        map.set(groupId, newNoteList);
-    }
-    console.log(map.keys);
+    } 
     res.send("Response added Successfully");
 };
 
-export const getFavourites = (req,res) =>{
-    for(let i=0; i<notes.length; i++)
-    if (notes[i].favourites==1){
-        res.send(notes[i]);
-    }
-    
-};
-
-export const addNoteToFavouriteList= (req,res) =>{
-    const note= req.body;
-    // note.favourite= true;
-
-};
-
+      
 export const deleteGroup=(req,res)=>{
-    let idnew=req.params.id;
-    
-    for(let i=0;i<groups.length;i++)
-    {   
-       if(idnew==":"+groups[i].id)
-         
-             groups.splice(i,1);
+    let groupId = String(req.params.id).slice(1);
+    for (let i = 0; i < groups.length ; i++) {   
+        if(groupId === groups[i].id) {
+            groups.splice(i, 1);
+        }
+    }
+    map.delete(groupId);
+    return res.json({success:200, message:'The selected Group has been deleted'})
+}
+   
+export const deleteNote = (req,res) => {
+    let noteId = String(req.params.id).slice(1);
+    let groupId = String(req.params.groupId).slice(1);
 
-            }
-            return res.json({success:200, message:'The selected Group has been deleted'})
-            }
-        
-        
-export const deleteNote=(req,res)=>{
-    let noteId=String(req.params.id).slice(1);
-    let groupId= String(req.params.groupId).slice(1);
-console.log(noteId)
-console.log(groupId)
-            for(let i=0; i< map.get(groupId).length; i++)
-            {
-                if(noteId===map.get(groupId)[i].id)
-                map.get(groupId).splice(i,1);
-           }
-           
-           for(let i = 0; i < groups.length; i++) {
-            let group = groups[i];
-    
-            if (group.id === groupId ){
-               group.count = group.count - 1; 
-            }
-            groups[i] = group;
+    for (let i=0; i< map.get(groupId).length; i++) {
+        if (noteId === map.get(groupId)[i].id) {
+            map.get(groupId).splice(i,1);
         }
-       return res.json({success:200, message:'The selected Note has been deleted'})
-   }
-   
-   
-   export const updateGroup=(req,res)=>{
-       let idnew=req.params.id;
-       
-       for(let i=0;i<groups.length;i++)
-       {
-        if(idnew==":"+groups[i].id)
-             groups.splice(i,1);
+    }
+        
+    for (let i = 0; i < groups.length; i++) {
+        let group = groups[i];
+        if (group.id === groupId) {
+            group.count = group.count - 1; 
         }
-     let group=req.body;
-     groups.push({...group, id: idnew});
-    return res.json({success:200, message:'The selected Group has been Updated'})
+        groups[i] = group;
+    }
+    return res.json({success:200, message:'The selected Note has been deleted'})
+}
+   
+
+export const updateGroup = (req,res) => {
+    let groupId = String(req.params.id).slice(1);
+    for(let i = 0; i < groups.length; i++) {
+    if(groupId === groups[i].id){
+        groups.splice(i,1); 
+        let group = req.body;
+        groups.splice(i,0,{...group, id: groupId, count: map.get(groupId).length});
+        break
+     }    
+    }
+    res.send(groups)
 }
 
 
-export const updateNote=(req,res)=>{
-    let idnew=req.params.id;
-    for(let i=0;i<groups.length;i++)
-    {
-       if(idnew==":"+groups[i].id)
-         
-             groups.splice(i,1);
- 
+export const updateNote = (req,res)=>{
+    let idnew = String(req.params.id).slice(1);
+    for(let i=0;i<groups.length;i++) {
+        if(idnew === groups[i].id) {
+            groups.splice(i,1);
         }
-     let group=req.body;
-     groups.push({...group, id: idnew});
-
+    }
+    let group = req.body;
+    groups.push({...group, id: idnew});
 
     return res.json({success:200, message:'The selected Note has been Updated'})
 }
-        
+
+export const getGroupName =  (req,res) => { 
+    let groupId = String(req.params.id).slice(1);
+    let name = '';
+
+    for (let i = 0; i < groups.length; i++) {
+        const group = groups[i]; 
+        if (group.id === groupId) {
+            name = group.title;
+            break
+        }
+    }
+    
+    return res.json({name: name});
+}
--- routes/groups.js
+++ routes/groups.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { createGroup, createNote, getFavourites, getGroups, getNotes, deleteGroup, deleteNote, 
-    updateGroup, updateNote, addNoteToFavouriteList, getNotesCount } from "../controller/groups.js"
+import { createGroup, createNote, getGroups, getNotes, deleteGroup, deleteNote, 
+    updateGroup, updateNote, getNotesCount, getGroupName } from "../controller/groups.js"
 
 const router = express.Router();
 
@@ -8,8 +8,6 @@ router.get("/groups", getGroups);
 router.post("/group", createGroup);
 router.get("/notes:id", getNotes);
 router.post("/note:id",createNote);
-router.get("/favourites", getFavourites);
-router.get("/favourites:id", addNoteToFavouriteList);
 
 router.delete( `/groups:id`, deleteGroup);
 router.delete( `/notes:id/group:groupId`, deleteNote);
@@ -18,5 +16,6 @@ router.put( `/groups:id`, updateGroup);
 router.put( `/notes:id`, updateNote);
 
 router.get("/notes-count:id", getNotesCount);
+router.get("/group:id", getGroupName);
 
 export default router;
